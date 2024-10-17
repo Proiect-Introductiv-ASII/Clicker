a8e60ac755168830886b8725a5760b42807a414d
alexandruioan20 <ialexmoraru@yahoo.ro>
2024-10-17 13:09:32 +0300

    pagina dashboard test

---
+++ clicker/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+
+  return (
+    <div> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse nihil voluptatem quis praesentium odit nam, explicabo rerum eius, sunt minima dolorum dolor, id maxime. Error officia eos obcaecati ex eveniet? </div>
+  )
+}
+
+export default page; 
