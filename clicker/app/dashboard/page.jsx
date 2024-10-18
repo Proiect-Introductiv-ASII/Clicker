f1551a61779da82add036c561204d50e0494de5c
EdiFarcas <farcas.edi@gmail.com>
2024-10-18 11:54:04 +0300

    NU merge index-ul.

--- clicker/app/dashboard/page.jsx
+++
@@ -1,8 +0,0 @@
-const page = () => {
-
-  return (
-    <div> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse nihil voluptatem quis praesentium odit nam, explicabo rerum eius, sunt minima dolorum dolor, id maxime. Error officia eos obcaecati ex eveniet? </div>
-  )
-}
-
-export default page; g
