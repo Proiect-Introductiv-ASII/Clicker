787280d71f6e334b15f8fa8f8d06bf9584ddde0e
EdiFarcas <farcas.edi@gmail.com>
2024-10-20 01:03:46 +0300

    Update style and funcionality
    Adaugat incrementare la nr de clickuri. + floating points pe langa click + styling oleaca.

--- clicker/app/game/page.jsx
+++ clicker/app/game/page.jsx
@@ -6,8 +6,22 @@ const Game = () => {
   const [points, setPoints] = useState(0);
   const [pointsPerClick, setPointsPerClick] = useState(1);
   const [pointsPerSecond, setPointsPerSecond] = useState(0);
+  const [pointsBoost, setPointsBoost] = useState(0);
+  const [upgradeClickcost, setUpgradeClickCost] = useState(10);
+  const [floatPoints, setFloatPoints] = useState([]);
 
-  const handleClick = () => setPoints(points + pointsPerClick);
+
+  const handleClick = () => {
+    setPoints(points + pointsPerClick);
+    
+    const newFloat = { id: Date.now(), value: '+ ' + pointsPerClick };
+    setFloatPoints([...floatPoints, newFloat]);
+
+    // Remove floating point after animation
+    setTimeout(() => {
+      setFloatPoints(floatPoints.filter((f) => f.id !== newFloat.id));
+    }, 1000);
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,9 +31,10 @@ const Game = () => {
   }, [pointsPerSecond]);
 
   const handleUpgradeClick = () => {
-    if (points >= 10) { // Spend 10 points for an upgrade
-      setPoints(points - 10);
+    if (points >= upgradeClickcost) { // Spend upgradeClickcost points for an upgrade
+      setPoints(points - upgradeClickcost);
       setPointsPerClick(pointsPerClick + 1); // Increase points per clicks
+      setUpgradeClickCost(Math.round(upgradeClickcost * 1.45));
     }
   }
 
@@ -29,6 +44,14 @@ const Game = () => {
       setPointsPerSecond(pointsPerSecond + 1); // Increase points per second
     }
   };
+
+  const handleBoost = () => {
+    if (points >= 50) {
+      setPoints(points - 50);
+      setPointsBoost(pointsBoost + 1);
+      setPointsPerSecond(pointsPerSecond * 2);
+    }
+  }
   
   return (
     <div>
@@ -36,10 +59,15 @@ const Game = () => {
       <h1>Your Points: {points}</h1>
       <p></p>
       <button onClick={handleClick}>Click for {pointsPerClick} points</button>
+      {floatPoints.map((fp) => (
+        <span key={fp.id} className="floating-points">{fp.value}</span>
+      ))}
       <p></p>
-      <button onClick={handleUpgradeClick}>Upgrade Click (Cost: 10 Points)</button>
+      <button onClick={handleUpgradeClick}>Upgrade Click (Cost: {upgradeClickcost} Points)</button>
       <p></p>
       <button onClick={handleUpgradeAutoClick}>Upgrade Auto-Click (Cost: 50 Points)</button>
+      <p></p>
+      <button onClick={handleBoost}>Upgrade Boost (Cost: 50 Points)</button>
       <p></p>
       <h1>Auto-points/sec: {pointsPerSecond}</h1>
     </div>
