4c0a0bf3f555f3b57089bead786b50382032c069
EdiFarcas <farcas.edi@gmail.com>
2024-10-21 16:08:48 +0300

    Home page style

--- clicker/app/page.jsx
+++ clicker/app/page.jsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div> 
-      <Link href = "login"> Login </Link>
-      <br />
-      <Link href = "register"> Register </Link>
+    <div className="home-container">
+      <div className="home-links">
+        <Link href="login">
+          <p className="link-button">Login</p>
+        </Link>
+        <Link href="register">
+          <p className="link-button">Register</p>
+        </Link>
+      </div>
     </div>
   );
 }
